f781fe880868f3e2f7c26f9f624cb29dd9b44546
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-04-27 19:30:52 -0700

    search assets and download them

--- cmd/run.go
+++ cmd/run.go
@@ -2,12 +2,24 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"os"
 	"time"
 
+	"github.com/go-immich/immich"
+	"github.com/google/uuid"
+	"github.com/oapi-codegen/runtime/types"
 	"github.com/rs/zerolog"
 	"github.com/urfave/cli/v3"
 	"golang.org/x/sync/errgroup"
 )
+
+const api_key_header_name = "x-api-key"
+
+func Ptr[T any](v T) *T {
+	return &v
+}
 
 func runCommand() *cli.Command {
 	return &cli.Command{
@@ -15,7 +27,20 @@ func runCommand() *cli.Command {
 		Aliases: []string{"r"},
 		Usage:   "run the frame refresher which sync deletes and if necessary refreshes the daily images",
 		Action:  runAction(),
-		Flags:   []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "immich-url",
+				Usage:    "The URL of the immmich server including the scheme",
+				Sources:  cli.EnvVars("IMMICH_URL"),
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "immich-api-key",
+				Usage:    "The API key to connect to Immich",
+				Sources:  cli.EnvVars("IMMICH_API_KEY"),
+				Required: true,
+			},
+		},
 	}
 }
 
@@ -42,6 +67,72 @@ func runAction() cli.ActionFunc {
 		g.Go(func() error {
 			return autoMaxProcs(ctx, 30*time.Second, logger)
 		})
+
+		// TODO: ... implementation ...
+
+		opts := []immich.ClientOption{
+			immich.WithRequestEditorFn(func(_ context.Context, req *http.Request) error {
+				req.Header.Set(api_key_header_name, cmd.String("immich-api-key"))
+
+				return nil
+			}),
+		}
+
+		u := cmd.String("immich-url") + "/api"
+
+		c, err := immich.NewClientWithResponses(u, opts...)
+
+		if err != nil {
+			return err
+		}
+
+		srr, err := c.SearchRandomWithResponse(ctx, immich.SearchRandomJSONRequestBody{
+			PersonIds: Ptr([]types.UUID{
+				uuid.MustParse("f1a4d1c1-1ff1-4a48-93bd-21f45b23b135"),
+				uuid.MustParse("796540fe-64f7-4360-8a06-7b4c1ed5ab46"),
+			}),
+			Type: Ptr(immich.IMAGE),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if srr.HTTPResponse.StatusCode != http.StatusOK {
+			return fmt.Errorf("request to get random assets failed with body :%s", string(srr.Body))
+		}
+
+		out, err := os.MkdirTemp("", "assets")
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("output directory is %s\n", out)
+
+		var cnt uint16
+		for _, asset := range *srr.JSON200 {
+			ar, err := c.ViewAssetWithResponse(ctx, uuid.MustParse(asset.Id), &immich.ViewAssetParams{
+				Size: Ptr(immich.AssetMediaSizeFullsize),
+			})
+			if err != nil {
+				return err
+			}
+
+			f, err := os.Create(out + "/" + asset.OriginalFileName)
+			if err != nil {
+				return err
+			}
+
+			if _, err := f.Write(ar.Body); err != nil {
+				return err
+			}
+
+			cnt++
+
+			if cnt == 10 {
+				break
+			}
+		}
 
 		// TODO: ... implementation ...
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kalbasit/immich-frame-sync
 go 1.23.7
 
 require (
+	github.com/go-immich/immich v0.0.0-20250428001727-5ce3e512faa1
 	github.com/kalbasit/ncps v0.1.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/rs/zerolog v1.34.0
@@ -24,6 +25,7 @@ require (
 )
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -31,6 +33,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/oapi-codegen/runtime v1.1.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/otel/metric v1.35.0 // indirect
